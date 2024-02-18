dedeba84cf11617ca02efd41c0e8e3544cef7f1d
Heiko John <healjo@hotmail.com>
2024-02-18 12:16:45 +0100

    JS refractored

--- js/main.js
+++ js/main.js
@@ -1,37 +1,31 @@
 "use strict";
 
-// Color Mode Switcher
-const colorSwitchBtn = document.getElementById("cmPref");
-
-// Set Origin
-const currentMode = document.documentElement.getAttribute("data-mode", modeToSet);
-
-const label = document.getElementById("cm-label");
-
-if (currentMode === "dark") {
-    colorSwitchBtn.setAttribute("aria-checked", "false");
-    label.innerHTML = "Dark Mode";
-}  else {
-    colorSwitchBtn.setAttribute("aria-checked", "true");
-    label.innerHTML = "Light Mode";
-}
-
-
-const changeCheckbox = (e) => {
+// Checks on user interaction and changes and sets state with new value
+const handleColorMode = (e) => {
+    
     // Keycode
     const spacebarKeyCode = 32;
-    // Checkbox
+    
+    // Checkbox clicked
     const switchBtn = e.target;
     const checked = switchBtn.getAttribute("aria-checked");
-    // Label
+    
+    // Label of clicked checkbox
     const labelId = switchBtn.getAttribute("aria-labelledby");
     const label = document.getElementById(labelId);
+    // Content of label
     const mode = label.innerHTML;
-    // Color Mode
+    
+    // Current color Mode
     const rootElement = document.querySelector("html");
     const currentValue = rootElement.getAttribute("data-mode");
+    
+    // Variable to change mode
     let newValue = "";
 
+    // 1. Check on code of pressed key
+    // 2. Check on "aria-checked" && content of label && currentValue
+    // 3. Do the opposite of 2.
     if (e.keyCode && e.keyCode !== spacebarKeyCode) {
         return;
     } else if (checked === "false" && mode === "Dark Mode" && currentValue === "dark") {
@@ -44,11 +38,37 @@ const changeCheckbox = (e) => {
         switchBtn.setAttribute("aria-checked", "false");
     };
 
+    // Set new value of color mode
     rootElement.setAttribute("data-mode", newValue);
-
+    // Set new value to the localStorage
     localStorage.setItem("mode", newValue);
 
 };
 
-colorSwitchBtn.addEventListener("click", changeCheckbox);
-colorSwitchBtn.addEventListener("keydown", changeCheckbox);
+// Gets and sets current state of the color mode switcher
+// based on the preferences of the user (set on webpage)
+const initColorModeSwitcher = () => {
+
+    // Button
+    const colorSwitchBtn = document.getElementById("cmPref");
+    // Label
+    const label = document.getElementById("cm-label");
+    // Current mode
+    const currentMode = document.documentElement.getAttribute("data-mode", modeToSet);
+
+    // Check current mode and set state of btn and label 
+    if (currentMode === "dark") {
+        colorSwitchBtn.setAttribute("aria-checked", "false");
+        label.innerHTML = "Dark Mode";
+    }  else {
+        colorSwitchBtn.setAttribute("aria-checked", "true");
+        label.innerHTML = "Light Mode";
+    };
+
+    // Set Eventlistener for specific user interaction
+    colorSwitchBtn.addEventListener("click", handleColorMode);
+    colorSwitchBtn.addEventListener("keydown", handleColorMode);
+
+};
+
+initColorModeSwitcher();
